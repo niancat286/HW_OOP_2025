8c3ee67e207037f85278fe3358ccf57607a30e94
Yaroslava <yakrenevych@gmail.com>
2025-03-09 00:41:43 +0200

    add Trapeze class

--- HW_06/utilts.py
+++ HW_06/utilts.py
@@ -29,6 +29,27 @@ class Rectangle:
         return self._a * self._b
 
 
+class Trapeze:
+    def __init__(self, a, b, c, d):
+        self._a = a
+        self._b = b
+        self._c = c
+        self._d = d
+
+
+    def get_perimeter(self):
+        return self._a + self._b + self._c + self._d
+
+    def get_square(self):
+        if self._a == self._b:  # Якщо це рівнобічна трапеція
+            h = math.sqrt(self._c ** 2 - ((self._b - self._a) / 2) ** 2)
+        else:
+            num = (-self._a + self._b + self._c + self._d) * (self._a - self._b + self._c + self._d) * (self._a - self._b + self._c - self._d) * (self._a - self._b - self._c + self._d)
+            h = math.sqrt(num) / (2 * abs(self._a - self._b))
+
+        return 0.5 * (self._a + self._b) * h
+
+
 if __name__ == '__main__':
     t1 = Triangle(4,9,11)
     print(t1.get_perimeter())
@@ -37,3 +58,7 @@ if __name__ == '__main__':
     rec1 = Rectangle(4,5)
     print(rec1.get_perimeter())
     print(rec1.get_square())
+
+    trap1 = Trapeze(10, 6, 5, 7)
+    print(trap1.get_perimeter())
+    print(trap1.get_square())
