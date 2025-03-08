b3ed8a33350388f94e8fdb11608b7485528f1777
Yaroslava <yakrenevych@gmail.com>
2025-03-09 00:15:04 +0200

    add rectangle class

--- HW_06/utilts.py
+++ HW_06/utilts.py
@@ -17,7 +17,23 @@ class Triangle:
         return math.sqrt(self.get_perimeter()/2*(self.get_perimeter()/2 - self._a)*(self.get_perimeter()/2 - self._b)*(self.get_perimeter()/2 - self._c))
 
 
+class Rectangle:
+    def __init__(self, a, b):
+        self._a = a
+        self._b = b
+
+    def get_perimeter(self):
+        return (self._a + self._b) * 2
+
+    def get_square(self):
+        return self._a * self._b
+
+
 if __name__ == '__main__':
-    t1 = Triangle(3,4,5)
+    t1 = Triangle(4,9,11)
     print(t1.get_perimeter())
     print(t1.get_square())
+
+    rec1 = Rectangle(4,5)
+    print(rec1.get_perimeter())
+    print(rec1.get_square())
