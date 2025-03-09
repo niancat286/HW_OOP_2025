4ca9890a10d069797a55b8c18593e0310472fa2f
Yaroslava <yakrenevych@gmail.com>
2025-03-09 22:25:26 +0200

    update

--- HW_06/main.py
+++ HW_06/main.py
@@ -1,53 +1,65 @@
 from utilts import *
 
 
-def check_file(file_path):
+def read_file(file_path):
+    figs = []
     with open(file_path, "r") as f:
-        max_perimeter = 0
-        max_perimeter_fig = []
-        max_square = 0
-        max_square_fig = []
-        figs = []
         for line in f:
             tmp = line.strip().split()
-            if all(int(x) != 0 for x in tmp[1:]):
+            # if all((int(x) is not None) and (int(x) != 0) for x in tmp[1:]):
+            if len(tmp) > 0:
                 try:
+                    data = [int(x) for x in tmp[1:]]
                     if tmp[0] == 'Triangle':
-                        fig = Triangle(*(int(x) for x in tmp[1:]))
+                        a, b, c = data
+                        fig = Triangle(a, b, c)
                     elif tmp[0] == 'Rectangle':
-                        fig = Rectangle(*(int(x) for x in tmp[1:]))
+                        a, b = data
+                        fig = Rectangle( a, b )
                     elif tmp[0] == 'Trapeze':
-                        fig = Trapeze(*(int(x) for x in tmp[1:]))
+                        a, b, c, d = data
+                        fig = Trapeze(a, b, c, d)
                     elif tmp[0] == 'Parallelogram':
-                        fig = Parallelogram(*(int(x) for x in tmp[1:]))
+                        a, b, h = data
+                        fig = Parallelogram(a, b, h)
                     elif tmp[0] == 'Circle':
-                        fig = Circle(*(int(x) for x in tmp[1:]))
+                        r = data[0]
+                        fig = Circle(r)
 
                     figs.append(fig)
 
-                    tmp_perimeter = fig.get_perimeter()
-                    if tmp_perimeter is not None and tmp_perimeter > max_perimeter:
-                        max_perimeter = tmp_perimeter
-                        max_perimeter_fig = tmp
-
-                    tmp_square = fig.get_square()
-                    if tmp_square is not None and tmp_square > max_square:
-                        max_square = tmp_square
-                        max_square_fig = tmp
-
-                except Exception as e:
+                except ValueError as e:
                     print(f"Помилка при створенні фігури {tmp[0]}: {e}")
 
-        print(f'figure with max perimeter {max_perimeter:.3f} is {max_perimeter_fig}')
-        print(f'figure with max square {max_square:.3f} is {max_square_fig}')
+    return figs
+
+
+def chec_data(figs):
+    max_perimeter = 0
+    max_perimeter_fig = []
+    max_square = 0
+    max_square_fig = []
+    for fig in figs:
+        tmp_perimeter = fig.get_perimeter()
+        if tmp_perimeter is not None and tmp_perimeter > max_perimeter:
+            max_perimeter = tmp_perimeter
+            # max_perimeter_fig = tmp
+
+        tmp_square = fig.get_square()
+        if tmp_square is not None and tmp_square > max_square:
+            max_square = tmp_square
+            # max_square_fig = tmp
+
+    print(f'figure with max perimeter {max_perimeter:.3f} is {max_perimeter_fig}')
+    print(f'figure with max square {max_square:.3f} is {max_square_fig}')
 
 
 if __name__ == '__main__':
 
+    figs = read_file('input01.txt')
+    for fig in figs:
+        print(f"{fig}")
 
 
-    check_file('input01.txt')
     # check_file('input02.txt')
     # check_file('input03.txt')
-
-
--- HW_06/utilts.py
+++ HW_06/utilts.py
@@ -3,32 +3,33 @@ import math
 
 class Triangle:
     def check_triangle_existence(self, a, b, c):
-        return a + b > c and a + c > b and b + c > a
+        return (a > 0 and b > 0 and c > 0 and
+                a + b > c and a + c > b and b + c > a)
 
     def __init__(self, a, b, c):
         if not self.check_triangle_existence(a, b, c):
-            self._a = self._b = self._c = None
-            return
+            raise ValueError("Triangle doesn't exist")
 
         self._a = a
         self._b = b
         self._c = c
 
     def get_perimeter(self):
-        if None in [self._a, self._b, self._c]:
-            return None
         return self._a + self._b + self._c
 
     def get_square(self):
-        if None in [self._a, self._b, self._c]:
-            return None
-
         p = self.get_perimeter() / 2
         return math.sqrt(p * (p - self._a) * (p - self._b) * (p - self._c))
+
+    def __str__(self):
+        return f"Triangle {self._a}, {self._b}, {self._c}, perimetr = {self.get_perimeter()}, square = {self.get_square()}"
 
 
 class Rectangle:
     def __init__(self, a, b):
+        if a <= 0 or b <= 0:
+            raise ValueError("Rectangle doesn't exist")
+
         self._a = a
         self._b = b
 
@@ -38,9 +39,26 @@ class Rectangle:
     def get_square(self):
         return self._a * self._b
 
+    def __str__(self):
+        return f"Rectangle {self._a}, {self._b}, perimetr = {self.get_perimeter()}, square = {self.get_square()}"
+
 
 class Trapeze:
+
+    def check_existance(self, a, b, c, d):
+        if a <= 0 or b <= 0 or c <= 0 or d <= 0:
+            return False
+
+        if (a + b > c + d) and (a + c > b + d) and (a + d > b + c) and (b + c > a + d):
+            return True
+
+        return False
+
+
     def __init__(self, a, b, c, d):
+        if not self.check_existance(a, b, c, d):
+            raise ValueError("Trapeze doesn't exist")
+
         self._a = a
         self._b = b
         self._c = c
@@ -50,24 +68,31 @@ class Trapeze:
         return self._a + self._b + self._c + self._d
 
     def get_square(self):
-        if self._a == self._b:  # Якщо це рівнобічна трапеція
-            try:
-                h = math.sqrt(self._c ** 2 - ((self._b - self._a) / 2) ** 2)
-                if h < 0:
-                    return None
-            except ValueError:
-                return None
-        else:
-            num = (-self._a + self._b + self._c + self._d) * (self._a - self._b + self._c + self._d) * (self._a - self._b + self._c - self._d) * (self._a - self._b - self._c + self._d)
-            if num < 0:
-                return None
-            h = math.sqrt(num) / (2 * abs(self._a - self._b))
+        a, b, c, d = self._a, self._b, self._c, self._d
 
-        return 0.5 * (self._a + self._b) * h
+        # Формула для знаходження висоти
+        h = (math.sqrt((-a + b + c + d) * (a - b + c + d) * (a - b + c - d) * (a - b - c + d))) / (2 * abs(a - b))
+
+        # Формула площі трапеції
+        return ((a + b) / 2) * h
+
+
+
+    def __str__(self):
+        return f"Trapeze {self._a}, {self._b}, {self._c},  {self._d}, perimetr = {self.get_perimeter()}, square = {self.get_square()}"
 
 
 class Parallelogram:
+    def check_existance(self, a, b, h):
+        if a <= 0 or b <= 0 or h <= 0:
+            return False
+
+        return True
+
     def __init__(self, a, b, h):
+        if not self.check_existance(a, b, h):
+            raise ValueError("Parallelogram doesn't exist")
+
         self._a = a
         self._b = b
         self._h = h
@@ -78,9 +103,15 @@ class Parallelogram:
     def get_square(self):
         return max(self._a * self._h, self._b * self._h)
 
+    def __str__(self):
+        return f"Parallelogram {self._a}, {self._b}, {self._h},  perimetr = {self.get_perimeter()}, square = {self.get_square()}"
+
 
 class Circle:
     def __init__(self, r):
+        if r <= 0:
+            raise ValueError("Circle doesn't exist")
+
         self._r = r
 
     def get_perimeter(self):
@@ -89,24 +120,33 @@ class Circle:
     def get_square(self):
         return math.pi * self._r ** 2
 
+    def __str__(self):
+        return f"Circle {self._r},  perimetr = {self.get_perimeter()}, square = {self.get_square()}"
+
 
 if __name__ == '__main__':
     t1 = Triangle(4, 9, 11)
     print(t1.get_perimeter())
     print(t1.get_square())
+    print(t1)
 
     rec1 = Rectangle(4, 5)
     print(rec1.get_perimeter())
     print(rec1.get_square())
+    print(rec1)
 
     trap1 = Trapeze(10, 6, 5, 7)
     print(trap1.get_perimeter())
     print(trap1.get_square())
+    print(trap1)
 
     par1 = Parallelogram(8, 6, 4)
     print(par1.get_perimeter())
     print(par1.get_square())
+    print(par1)
 
     c1 = Circle(6)
     print(c1.get_perimeter())
     print(c1.get_square())
+
+    print(c1)
