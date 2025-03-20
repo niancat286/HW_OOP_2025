dc2b508b90e217a0d6a0a2a7d871b0ed175ea654
Yaroslava <yakrenevych@gmail.com>
2025-03-20 23:44:01 +0200

    Trapeze.py HW7

---
+++ HW_07/Trapeze.py
@@ -0,0 +1,49 @@
+import math
+
+from Figure2D import Figure2D
+
+
+class Trapeze(Figure2D):
+
+    def check_existance(self, a, b, c, d):
+        if a <= 0 or b <= 0 or c <= 0 or d <= 0:
+            return False
+
+        if (c + d > abs(a - b)) and (c + abs(a - b) > d) and (d + abs(a - b) > c):
+            return True
+
+        return False
+
+    def __init__(self, a, b, c, d):
+        super().__init__()
+        if not self.check_existance(a, b, c, d):
+            raise ValueError("Trapeze doesn't exist")
+
+        self._a = a
+        self._b = b
+        self._c = c
+        self._d = d
+
+    def perimetr(self):
+        return self._a + self._b + self._c + self._d
+
+    def square(self):
+        a, b, c, d = self._a, self._b, self._c, self._d
+
+        # Формула для знаходження висоти
+        h = (math.sqrt((-a + b + c + d) * (a - b + c + d) * (a - b + c - d) *
+                       (a - b - c + d))) / (2 * abs(a - b))
+
+        # Формула площі трапеції
+        return ((a + b) / 2) * h
+
+    def volume(self):
+        return self.square()
+
+    def __str__(self):
+        return f"Trapeze {self._a}, {self._b}, {self._c},  {self._d}, volume = {self.volume()}"
+
+
+if __name__ == '__main__':
+    tr = Trapeze(10, 6, 5, 7)
+    print(tr)
